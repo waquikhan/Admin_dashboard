995ea480e705851b82003440060154803d0ec950
waquikhan <103071047+waquikhan@users.noreply.github.com>
2023-12-26 10:11:14 +0530

    added the menue section in the project

---
+++ src/Components/Menue/Menue.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+import "./Menue.scss"
+import { menu } from "../../data"
+
+const Menue = () => {
+  return (
+    <div className="menue">
+      {/* Menue item fetch with the map method. data is present insude the data file in the ./src */}
+        {menu.map(item=>(
+          <div className="item" key={item.id}>
+          <span className="title"> {item.title}</span>
+
+           {/* This is the sub Heading of menue come from the same array */}
+          {item.listItems.map(listItem=>(
+            <Link to="/" className="listItem" key={listItem.id}>
+            <img src={listItem.icon} alt="" />
+            <span className="listItemTitle">{listItem.title}</span>
+          </Link>
+        ))}
+        </div>
+        ))}
+    </div>
+  )
+}
+
+export default Menue
