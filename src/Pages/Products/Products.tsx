17b7558032f8a29c94478628e2f5ec778ccf2d0c
waquikhan <103071047+waquikhan@users.noreply.github.com>
2023-12-29 12:38:50 +0530

    added ProductPages into the Project

--- src/Pages/Products/Products.tsx
+++ src/Pages/Products/Products.tsx
@@ -1,8 +1,66 @@
+import { useState } from "react"
+import AddModalBox from "../../Components/AddModalBox/AddModalBox"
+import DataTable from "../../Components/DataTable/DataTable"
 import "./Products.scss" 
+import { GridColDef } from "@mui/x-data-grid";
+import { product } from "../../data";
+
+const columns: GridColDef [] = [
+  {field:"id", headerName:"ID", width:90},
+  {field:"img", headerName:"Image", width:100,
+    renderCell: (params)=>{
+      return <img src={params.row.img} alt="" />
+    },
+  },
+  {
+    field:"title",
+    type:"string",
+    headerName:"Title",
+    width:400
+  },
+  {
+    field:"color",
+    type:"string",
+    headerName:"Color",
+    width:150
+  },
+  {
+    field:"price",
+    type:"string",
+    headerName:"Price",
+    width:150
+  },
+  {
+    field:"producer",
+    type:"string",
+    headerName:"Producer",
+    width:150
+  },
+  {
+    field:"createAt",
+    type:"string",
+    headerName:"Create-At",
+    width:150
+  },
+  {
+    field:"inStock",
+    type:"boolean",
+    headerName:"In-Stock",
+    width:150
+  },
+];
+
+
 const Products = () => {
+  const[ open, setOpne] = useState(false)
   return (
     <div className="products">
-      Products
+      <div className="info">
+        <h1>Products</h1>
+        <button onClick={()=>setOpne(true)}>Add New Products</button>
+      </div>
+        <DataTable slug="product"  columns={columns} rows={product}  />
+        {open && <AddModalBox slug="product" columns={columns} setOpne={setOpne} />}
     </div>
   )
 }
